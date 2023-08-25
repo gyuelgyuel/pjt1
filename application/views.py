157b7ee80f12d3a13d139d095a54ae3684de16a0
gyuelgyuel <rockymail98@gmail.com>
2023-08-25 17:08:28 +0900

    update

--- application/views.py
+++ application/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
 
 # Create your views here.
+
+def index(request):
+    articles = Article.objects.all()
+
+    context = {
+        'articles': articles,
+    }
+
+    return render(request, 'index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form=ArticleForm()
+
+    context={
+        'form': form,
+    }
+    return render(request, 'form.html', context)
+        
+        
+def detail(request,id):
+    article = Article.objects.get(id=id)
+
+    comment_form = CommentForm()
+
+    context = {
+        'article': article,
+        'comment_form': comment_form
+    }
+
+    return render(request, 'detail.html', context)
+
+def comment_create(request, article_id):
+    comment_form = CommentForm(request.POST)
+
+    comment = comment_form.save(commit=False)
+
+    comment.article_id = article_id
+
+    comment.save()
+
+    return redirect('articles:detail', id=article_id)
